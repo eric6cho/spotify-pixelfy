741500a79b44c343943131dd2bf9c7108d6b8fab
Eric Cho <eric6cho.repo@gmail.com>
2022-09-25 03:24:04 -0400

    cleaned up backend removed server utils

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
-
 require('dotenv').config();
-const u = require('./scripts/utils.js');
 const path = require('path');
 const express = require("express");
 const SpotifyWebApi = require('spotify-web-api-node');
+const request = require('request');
 const app = express();
 
 const ISLOCAL = process.env.PORT===null||process.env.PORT===undefined;
@@ -27,7 +26,7 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get('/login', (req, res) => {
   const scopes = ['user-read-private','user-read-email','user-top-read'];
-  const state = u.getRandomStr();
+  const state = getRandomStr();
   const authURL = spotifyApi.createAuthorizeURL(scopes, state);
   
   res.cookie('spotify_auth_state',state);
@@ -72,39 +71,37 @@ app.get('/refresh_token', (req, res) => {
 });
 
 app.get('/api/get/server', (req, res) => {
-  res.json({url:SERVERHOST});
+  res.json({'url':SERVERHOST});
 }); 
 
 app.get('/api/get/artists', (req, res) => {
   spotifyApi.getMyTopArtists().then(
     (data) => res.json(data.body.items), 
-    (err) => console.log('Something went wrong!', err));
-}); 
-
-app.get('/api/set/access_token', (req, res) => {
-  spotifyApi.setAccessToken(req.query.access_token);
-  let data = {'access_token':spotifyApi.getAccessToken()};
-  res.json(data);
-}); 
-
-app.get('/api/get/access_token', (req, res) => {
-  let data = {'access_token':spotifyApi.getAccessToken()};
-  res.json(data);
+    (err) => console.log('ERROR GET ARTISTS:', err));
 }); 
 
 app.get('/api/get/profile', (req, res) => {
   spotifyApi.getMe().then(
     (data) => res.json(data.body), 
-    (err) => console.log('Something went wrong!', err));
+    (err) => console.log('ERROR GET PROFILE:', err));
 }); 
 
-app.get('/api/IM/wake_up',(req,res) => u.getUrlData(IMAGEMESHAPI).then(data=>res.json(data)));
+app.get('/api/get/access_token', (req, res) => {
+  res.json({'access_token':spotifyApi.getAccessToken()});
+}); 
+
+app.get('/api/set/access_token', (req, res) => {
+  spotifyApi.setAccessToken(req.query.access_token);
+  res.json({'access_token':spotifyApi.getAccessToken()});
+}); 
+
+app.get('/api/IM/wake_up',(req,res) => getUrlData(IMAGEMESHAPI).then(data=>res.json(data)));
 
 app.get('/api/IM/get/image',(req,res) => {
   const queryData = [['url', req.query.url]];
   const queryParams = new URLSearchParams(queryData).toString();
   let apiCall = IMAGEMESHAPI+'/get/image/quickEdit/?'+queryParams;
-  u.getUrlData(apiCall).then(data=>res.json(data));
+  getUrlData(apiCall).then(data=>res.json(data));
 });
   
 app.get('/', (req, res) => res.json({ message: 'Hello from the server on port '+PORT })); 
@@ -112,3 +109,14 @@ app.get('/', (req, res) => res.json({ message: 'Hello from the server on port '+
 app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../client/public', 'index.html'))); 
 
 app.listen(PORT, () => console.log(`SERVER: Listening in a ${ISLOCAL?'local':'production'} environment on ${PORT}`));
+
+const getRandomStr = (length=16) => {
+  let str = '';
+  let s = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i=0; i<length; i++) 
+    str += s.charAt(Math.floor(Math.random()*s.length));
+  return str;
+};
+
+const getUrlData =async (url) => new Promise (resolve => 
+  request.get(url, (error, response, body) => resolve(JSON.parse(body))));
